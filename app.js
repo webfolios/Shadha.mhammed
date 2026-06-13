eff25c74ae7a6a57e16dc99d078d18c407f6ae6e
webfolios <80982752+webfolios@users.noreply.github.com>
2026-06-13 18:17:55 +0300

    Delete app.js

--- app.js
+++
@@ -1,5 +0,0 @@
-
-document.querySelectorAll('.card').forEach(c=>{
-c.addEventListener('mouseenter',()=>c.style.transform='translateY(-8px)');
-c.addEventListener('mouseleave',()=>c.style.transform='translateY(0)');
-});
